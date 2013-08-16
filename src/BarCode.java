c1373d0887803a80007fcccaa7ca3f46030a6836
malith <malithn@hsenidmobile.com>
2013-08-16 17:29:24 +0530

    Barcode creator added

---
+++ src/BarCode.java
@@ -0,0 +1,24 @@
+import com.onbarcode.barcode.EAN13;
+import com.onbarcode.barcode.ISBN;
+import com.onbarcode.barcode.ISSN;
+
+public class BarCode {
+
+    public static void main(String[] ar) {
+
+        ISSN issn = new ISSN();
+        ISBN isbn = new ISBN();
+        EAN13 ean13 = new EAN13();
+        issn.setData("123456789");
+        isbn.setData("123456789");
+        ean13.setData("000123456789");
+        try {
+            issn.drawBarcode("/home/malith/malithBarCodeISSN.jpg");
+            isbn.drawBarcode("/home/malith/malithBarCodeISBN.jpg");
+            ean13.drawBarcode("/home/malith/malithBarCodeEAN13.jpg");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
